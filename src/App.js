8c15210d10c6fdd25462f0c98990e727fc061612
xgaia <xgaia@gmx.com>
2021-04-18 11:39:54 +0200

    circula chessboard && docs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import './App.css'
 import antImg from './ant.png'
-import { Container, Button } from "reactstrap"
+import { Container, Button, FormGroup, Label, CustomInput } from "reactstrap"
 
 /**
  * Longton Ant simulation single page app 
@@ -24,10 +24,13 @@ export default class App extends Component {
       antPosition: [0, 0],
       antDirection: 0,
       timer: null,
+      circular: false
     }
     this.simulate = this.simulate.bind(this)
     this.initSimulation = this.initSimulation.bind(this)
     this.stop = this.stop.bind(this)
+    this.cropMax = this.cropMax.bind(this)
+    this.toggleCircular = this.toggleCircular.bind(this)
   }
 
   initMatrix() {
@@ -89,28 +92,48 @@ export default class App extends Component {
     const newDir = this.circularGet(this.compass, new_index)
 
     // New ant position
-    const newX = newDir === 0 ? this.cropMax(this.state.antPosition[0] - 1, this.state.x_len - 1) : 
-                                  newDir === 2 ? this.cropMax(this.state.antPosition[0] + 1, this.state.x_len - 1) : 
-                                  this.state.antPosition[0]
+    const newPos = this.state.circular ? this.getNewPosition(newDir, this.circularMax) : this.getNewPosition(newDir, this.cropMax)
 
-    const newY = newDir === 3 ? this.cropMax(this.state.antPosition[1] - 1, this.state.y_len - 1) :
-                                  newDir === 1 ? this.cropMax(this.state.antPosition[1] + 1, this.state.y_len - 1) : 
-                                  this.state.antPosition[1]
-
-    const new_pos = [newX, newY]
 
     // set state
     this.setState({
       matrix: newMatrix,
-      antPosition: new_pos,
+      antPosition: newPos,
       antDirection: newDir,
       timer: setTimeout(this.simulate, 500)
     })
   }
 
+  getNewPosition (dir, callback) {
+    /* get the position when ant can pass througt walls */
+    const newX = dir === 0 ? callback(this.state.antPosition[0] - 1, this.state.x_len - 1) : 
+                                  dir === 2 ? callback(this.state.antPosition[0] + 1, this.state.x_len - 1) : 
+                                  this.state.antPosition[0]
+
+    const newY = dir === 3 ? callback(this.state.antPosition[1] - 1, this.state.y_len - 1) :
+                                  dir === 1 ? callback(this.state.antPosition[1] + 1, this.state.y_len - 1) : 
+                                  this.state.antPosition[1]
+
+    return [newX, newY]
+
+  }
+
   circularGet (arr, n) {
     /* Get a value in a list, but circular if index is larger than the list */
     return arr[(n % arr.length + arr.length) % arr.length]
+  }
+
+  circularMax (val, max) {
+    /* if the value is larger the the max, return 0. If val is negative, return max */
+    if (val >= max) {
+      return 0
+    }
+
+    if (val <= 0) {
+      return max
+    }
+
+    return val
   }
 
   cropMax(val, max) {
@@ -145,17 +168,36 @@ export default class App extends Component {
     return false
   }
 
+  toggleCircular () {
+    this.setState({
+      circular: !this.state.circular,
+    })
+  }
 
   render() {
     /* Render the matrix and the ant */
     return (
-      <Container fluid className="app">
+      <Container fluid className="app py-3">
+      <h1 className="mb-3">Langton's Ant Simulation</h1>
       <Container>
+        <p>Langton's ant is a two-dimensional universal Turing machine with a very simple set of rules but complex emergent behavior. It was invented by Chris Langton in 1986 and runs on a square lattice of black and white cells.</p>
+        <p>Squares on a plane are colored variously either black or white. We arbitrarily identify one square as the "ant". The ant can travel in any of the four cardinal directions at each step it takes. The "ant" moves according to the rules below:</p>
+        <ul>
+          <li>At a white square, turn 90° clockwise, flip the color of the square, move forward one unit</li>
+          <li>At a black square, turn 90° counter-clockwise, flip the color of the square, move forward one unit</li>
+        </ul>
+        <p>Use the button to start, stop and reset the simulation. The switch allows the ant to cross or not the walls of the chessboard.</p>
+        <p>Sources are available on <a href="http://github.com/xgaia/langton-ant">GitHub</a> under <a href="https://github.com/xgaia/langton-ant/blob/main/LICENSE">AGPL-3.0</a> licence.</p>
+      </Container>
+
+      <Container className="center mb-3">
         <Button className="btn" onClick={this.simulate}>Start</Button>{" "}
         <Button className="btn" onClick={this.stop}>Stop</Button>{" "}
         <Button className="btn" onClick={this.initSimulation}>Reset</Button>
+        <CustomInput value={this.state.circular} onClick={this.toggleCircular} type="switch" id="switch" name="customSwitch" label="Circular chessboard" />
       </Container>
-      <div>
+
+      <div className={this.state.circular ? "circular" : "wall"}>
         {this.state.matrix.map((row, i) => (
           <div className="flex matrix-row" key={i}>
             {row.map((col, j) => (
